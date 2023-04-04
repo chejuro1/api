0cf83dfa8f395b4b5f0299375dc153e695472eb7
Jules Chendjou <julestl@yahoo.fr>
2023-04-04 17:54:33 -0400

    update database url

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
 client = MongoClient('MONGODB_URI')
 db = client['customer_db']
 customers = db.customers
